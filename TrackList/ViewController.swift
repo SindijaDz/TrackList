63601ac056ca979ed7f61194c8baecf4be2783c2
SindijaDz <sindijad@yahoo.com>
2020-09-02 18:38:10 +0300

    Initial Commit

---
+++ TrackList/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TrackList
+//
+//  Created by sindija.dzintare on 02/09/2020.
+//  Copyright © 2020 sindija.dzintare. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
